ba89e6d733ec5f4912ab6f9ccd932613d1c94624
Juri Bogatyrjow <juri.bogatyrjow@stud.hs-hannover.com>
2020-06-26 13:52:08 +0200

    AI

---
+++ Assets/Scripts/AI/AIContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FlightAce.interfaces;
+using UnityEngine;
+
+namespace FlightAce.AI
+{
+    public class AIContext : MonoBehaviour, IActorContext
+    {
+        [SerializeField] private float horizontalSpeed;
+        [SerializeField] private float verticalSpeed;
+        [SerializeField] private float amplitude;
+        
+        public IMovementInput MovementInput { get; private set; }
+        public IWeaponInput WeaponInput { get; private set; }
+        void Awake()
+        {
+            MovementInput = new AIMovementInput(horizontalSpeed, verticalSpeed, amplitude);
+            WeaponInput = new AIWeaponInput();
+         
+        }
+
+    }
+ 
+}
---
+++ Assets/Scripts/AI/AIMovementInput.cs
@@ -0,0 +1,27 @@
+﻿using FlightAce.interfaces;
+using UnityEngine;
+
+namespace FlightAce.AI
+{
+    public class AIMovementInput: IMovementInput
+    {
+        public float horizontalSpeed { private get; set; }
+        public float verticalSpeed { private get; set; }
+        public float amplitude { private get; set; }
+
+        public AIMovementInput(float hori, float verti, float amp)
+        {
+            horizontalSpeed = hori;
+            verticalSpeed = verti;
+            amplitude = amp;
+        }
+        
+        private AIMovementInput _movementInput;
+        
+        public Vector3 GetInputVector()
+        {
+            var newPos = new Vector3(x: horizontalSpeed,y:  Mathf.Sin(Time.realtimeSinceStartup * verticalSpeed) * amplitude,z: 0);
+            return newPos;
+        }
+    }
+}
