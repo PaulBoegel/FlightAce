63327bedbc53240169992e3575302b221b871703
PaulBoegel <pboegelsack@gmail.com>
2020-06-26 14:07:29 +0200

    Added weapon mechanics for player aircraft

---
+++ Assets/Scripts/ActorCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ActorCollision : MonoBehaviour
+{
+    [SerializeField] private UnityEvent Collied;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Collied?.Invoke();
+    }
+}
--- Assets/Scripts/weapon/Weapon.cs
+++ Assets/Scripts/weapon/Weapon.cs
@@ -1,23 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using FlightAce.interfaces;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace FlightAce.weapon
 {
     [RequireComponent(typeof(IActorContext))]
     public class Weapon : MonoBehaviour
     {
-
+        
+        [Range(0, 100)]
+        [SerializeField] private float _fireDelay = 1;
         [SerializeField] private SpriteRenderer _muzzleFlash;
-
+        [SerializeField] private bool _debugMode = false;
+        
+        
         private IWeaponInput _weaponInput;
+        private GameObject _muzzle;
+        private bool _bulletIsLoaded;
 
         // Start is called before the first frame update
         void Start()
         {
             var context = GetComponent<IActorContext>();
             _weaponInput = context.WeaponInput;
-            StartCoroutine("MuzzleFlashAnimation");
+            _muzzle = _muzzleFlash.gameObject;
+            StartCoroutine("LoadBullet");
         }
 
         // Update is called once per frame
@@ -25,21 +34,49 @@ namespace FlightAce.weapon
         {
             if (_weaponInput.isFireing())
             {
-                _muzzleFlash.gameObject.SetActive(true);
+                _muzzle.SetActive(true);
+                if (_bulletIsLoaded)
+                {
+                    _muzzleFlash.enabled = true;
+                    _bulletIsLoaded = false;
+                    return;
+                }
+                
+                _muzzleFlash.enabled = false;
                 return;
             }
             
             _muzzleFlash.gameObject.SetActive(false);
         }
 
-        IEnumerator MuzzleFlashAnimation()
+        private void FixedUpdate()
+        {
+            if (_weaponInput.isFireing() && _bulletIsLoaded)
+            {
+                FireRaycast();
+            }
+        }
+
+        private void FireRaycast()
+        {
+            var hit = Physics2D.Raycast(_muzzle.transform.position, _muzzle.transform.right);
+            
+            if (_debugMode)
+                Debug.DrawRay(_muzzle.transform.position, _muzzle.transform.right * 100, Color.red, 1f);
+            
+            if (hit.collider != null)
+            {
+                if(_debugMode)
+                    Debug.Log(hit.transform.gameObject.name);
+            }
+        }
+        
+        IEnumerator LoadBullet()
         {
             while (true)
             {
-                yield return new WaitForSeconds(0.1f);
-                if (_weaponInput.isFireing())
-                    _muzzleFlash.enabled = !_muzzleFlash.enabled;  
-                
+                yield return new WaitForSeconds(_fireDelay * 0.01f);
+                _bulletIsLoaded = true;
             }
         }
     }
