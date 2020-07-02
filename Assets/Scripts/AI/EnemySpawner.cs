4a0c87a4237c7bd987b67c03ea4ced84c5dbed7d
Juri Bogatyrjow <juri.bogatyrjow@stud.hs-hannover.com>
2020-07-02 17:14:03 +0200

    New Status

---
+++ Assets/Scripts/AI/EnemySpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using FlightAce.AI;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+    private float randomY;
+    private Vector3 spawnPosition;
+    public float spawnRate = 2f;
+    private float nextSpawn = 0.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randomY = Random.Range(-3f, 3f);
+            spawnPosition = new Vector3(transform.position.x, randomY, 0);
+            var enemyObject = Instantiate(enemy, spawnPosition, Quaternion.identity);
+            var enemyContext = enemyObject.GetComponent<AIContext>();
+        }
+    }
+}
