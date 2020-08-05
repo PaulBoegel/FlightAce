a794f8343868a8ff2a2bf12f4983b3228973c3cc
PaulBoegel <pboegelsack@gmail.com>
2020-08-05 12:03:29 +0200

    Added moving background with image

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float speed;
+
+    private Vector3 startPOS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPOS = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Time.deltaTime * speed * new Vector3(-1, 0, 0));
+        if (transform.position.x < -19.17395f)
+        {
+            transform.position = startPOS;
+        }
+    }
+}
